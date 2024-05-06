b9fae54eac3adb016a618bc05a82fa7f4d265a28
parikshitbarua <parikshitb04@gmail.com>
2024-05-07 03:08:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recipe-shopping-app';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
